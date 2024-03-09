843393882f9f29a2ea621fa98edd711f0702214d
calves20 <157167670+calves20@users.noreply.github.com>
2024-03-08 18:33:45 -0800

    Update mmm.h

--- mmm.h
+++ mmm.h
@@ -1,16 +1,20 @@
 #ifndef MMM_H_
 #define MMM_H_
+//author @Curtis Alves
+#include <pthread.h>
 
-// shared globals
-extern unsigned int mode;
 extern unsigned int size, num_threads;
 extern double **A, **B, **SEQ_MATRIX, **PAR_MATRIX;
 
 void mmm_init();
-void mmm_reset(double **);
 void mmm_freeup();
-void mmm_seq();
-void* mmm_par(void *);
-double mmm_verify();
+void mmm_reset(double **matrix);
+double **matrix_allocate(int n);
+void matrix_fill(double **matrix, int n);
+void matrix_zero(double **matrix, int n);
+void mmm_seq(double **A, double **B, double **C, int n);
+void* mmm_par_worker(void *arg);
+void mmm_par(double **A, double **B, double **C, int n);
+double mmm_verify(double **SEQ_MATRIX, double **PAR_MATRIX, int n);
 
 #endif /* MMM_H_ */
